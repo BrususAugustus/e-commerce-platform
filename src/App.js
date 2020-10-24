2316b78af521839eb445dd43c11259963f76b662
unknown <yourEmail>
2020-10-24 13:38:50 +0200

    broken app into components, finished styling those components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import HomePage from "./components/homepage.component"
+import HomePage from "./components/pages/homepage/homepage.component";
+
 function App() {
   return (
     <div className="App">
